cebb962ece707e1bf9e2ddea6dd429c1ccec7263
DESKTOP-MUHRSKU\me <192253540@qq.com>
2017-07-03 19:33:17 +0800

    logisticsService

---
+++ src/main/java/com/yayiabc/http/mvc/service/LogisticsService.java
@@ -0,0 +1,12 @@
+package com.yayiabc.http.mvc.service;
+
+import java.util.HashMap;
+
+import com.sun.xml.internal.xsom.impl.scd.Iterators.Map;
+import com.yayiabc.common.utils.DataWrapper;
+import com.yayiabc.http.mvc.pojo.jpa.Ordera;
+
+public interface LogisticsService {    //
+     
+	HashMap<String,Ordera> queryLog(String phone,String itemId);
+}

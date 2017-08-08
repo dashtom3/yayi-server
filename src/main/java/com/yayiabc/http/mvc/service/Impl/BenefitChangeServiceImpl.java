5e779e70358c423c2eb095852edd2518802d8e47
yehu <15222004571@163.com>
2017-08-08 14:01:46 +0800

    1401提交

--- src/main/java/com/yayiabc/http/mvc/service/Impl/BenefitChangeServiceImpl.java
+++ src/main/java/com/yayiabc/http/mvc/service/Impl/BenefitChangeServiceImpl.java
@@ -185,6 +185,7 @@ public class BenefitChangeServiceImpl implements BenefitChangeService{
         BufferedOutputStream bos = null;
         try {
         	response.setHeader("Content-Disposition", "attachment;filename="+ URLEncoder.encode(fileName, "UTF-8"));
+            response.setHeader("Access-Control-Allow-Origin", "*");
         	ServletOutputStream out = response.getOutputStream();
             bis = new BufferedInputStream(is);
             bos = new BufferedOutputStream(out);

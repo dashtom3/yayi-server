88475d1b09105961d3e251f4e98fcab31390a02e
liqitian3344 <192253540@qq.com>
2017-08-30 17:26:48 +0800

    12321

---
+++ src/main/java/com/yayiabc/http/mvc/pojo/model/FinalList.java
@@ -0,0 +1,186 @@
+package com.yayiabc.http.mvc.pojo.model;
+
+public class FinalList {
+	private String itemId;
+	private String itemSKU;
+	private Double itemSkuPrice;
+	private Double  tiChen;
+	private Integer itemQb;
+	private Integer stockNum;
+	private Integer canUse;
+	private Integer itemValueId;
+	private String itemPropertyName;
+	private String itemPropertyInfo;
+	private String itemPropertyNameTwo;
+	private String itemPropertyTwoValue;
+	private String itemPropertyNameThree;
+	private String itemPropertyThreeValue;
+	private String itemPropertyFourName;
+	private String itemPropertyFourValue;
+	private String itemPropertyFiveName;
+	private String itemPropertyFiveValue;
+	private String itemPropertySixName;
+	private String itemPropertySixValue;
+    private String itemName;
+    private String itemType;//商品类型
+    private String itemBrandName;
+    private String picPath;
+	public String getItemId() {
+		return itemId;
+	}
+	public void setItemId(String itemId) {
+		this.itemId = itemId;
+	}
+	public String getItemSKU() {
+		return itemSKU;
+	}
+	public void setItemSKU(String itemSKU) {
+		this.itemSKU = itemSKU;
+	}
+	public Double getItemSkuPrice() {
+		return itemSkuPrice;
+	}
+	public void setItemSkuPrice(Double itemSkuPrice) {
+		this.itemSkuPrice = itemSkuPrice;
+	}
+	public Double getTiChen() {
+		return tiChen;
+	}
+	public void setTiChen(Double tiChen) {
+		this.tiChen = tiChen;
+	}
+	public Integer getItemQb() {
+		return itemQb;
+	}
+	public void setItemQb(Integer itemQb) {
+		this.itemQb = itemQb;
+	}
+	public Integer getStockNum() {
+		return stockNum;
+	}
+	public void setStockNum(Integer stockNum) {
+		this.stockNum = stockNum;
+	}
+	public Integer getCanUse() {
+		return canUse;
+	}
+	public void setCanUse(Integer canUse) {
+		this.canUse = canUse;
+	}
+	public Integer getItemValueId() {
+		return itemValueId;
+	}
+	public void setItemValueId(Integer itemValueId) {
+		this.itemValueId = itemValueId;
+	}
+	public String getItemPropertyName() {
+		return itemPropertyName;
+	}
+	public void setItemPropertyName(String itemPropertyName) {
+		this.itemPropertyName = itemPropertyName;
+	}
+	public String getItemPropertyInfo() {
+		return itemPropertyInfo;
+	}
+	public void setItemPropertyInfo(String itemPropertyInfo) {
+		this.itemPropertyInfo = itemPropertyInfo;
+	}
+	public String getItemPropertyNameTwo() {
+		return itemPropertyNameTwo;
+	}
+	public void setItemPropertyNameTwo(String itemPropertyNameTwo) {
+		this.itemPropertyNameTwo = itemPropertyNameTwo;
+	}
+	public String getItemPropertyTwoValue() {
+		return itemPropertyTwoValue;
+	}
+	public void setItemPropertyTwoValue(String itemPropertyTwoValue) {
+		this.itemPropertyTwoValue = itemPropertyTwoValue;
+	}
+	public String getItemPropertyNameThree() {
+		return itemPropertyNameThree;
+	}
+	public void setItemPropertyNameThree(String itemPropertyNameThree) {
+		this.itemPropertyNameThree = itemPropertyNameThree;
+	}
+	public String getItemPropertyThreeValue() {
+		return itemPropertyThreeValue;
+	}
+	public void setItemPropertyThreeValue(String itemPropertyThreeValue) {
+		this.itemPropertyThreeValue = itemPropertyThreeValue;
+	}
+	public String getItemPropertyFourName() {
+		return itemPropertyFourName;
+	}
+	public void setItemPropertyFourName(String itemPropertyFourName) {
+		this.itemPropertyFourName = itemPropertyFourName;
+	}
+	public String getItemPropertyFourValue() {
+		return itemPropertyFourValue;
+	}
+	public void setItemPropertyFourValue(String itemPropertyFourValue) {
+		this.itemPropertyFourValue = itemPropertyFourValue;
+	}
+	public String getItemPropertyFiveName() {
+		return itemPropertyFiveName;
+	}
+	public void setItemPropertyFiveName(String itemPropertyFiveName) {
+		this.itemPropertyFiveName = itemPropertyFiveName;
+	}
+	public String getItemPropertyFiveValue() {
+		return itemPropertyFiveValue;
+	}
+	public void setItemPropertyFiveValue(String itemPropertyFiveValue) {
+		this.itemPropertyFiveValue = itemPropertyFiveValue;
+	}
+	public String getItemPropertySixName() {
+		return itemPropertySixName;
+	}
+	public void setItemPropertySixName(String itemPropertySixName) {
+		this.itemPropertySixName = itemPropertySixName;
+	}
+	public String getItemPropertySixValue() {
+		return itemPropertySixValue;
+	}
+	public void setItemPropertySixValue(String itemPropertySixValue) {
+		this.itemPropertySixValue = itemPropertySixValue;
+	}
+	public String getItemName() {
+		return itemName;
+	}
+	public void setItemName(String itemName) {
+		this.itemName = itemName;
+	}
+	public String getItemType() {
+		return itemType;
+	}
+	public void setItemType(String itemType) {
+		this.itemType = itemType;
+	}
+	public String getItemBrandName() {
+		return itemBrandName;
+	}
+	public void setItemBrandName(String itemBrandName) {
+		this.itemBrandName = itemBrandName;
+	}
+	public String getPicPath() {
+		return picPath;
+	}
+	public void setPicPath(String picPath) {
+		this.picPath = picPath;
+	}
+	@Override
+	public String toString() {
+		return "FinalList [itemId=" + itemId + ", itemSKU=" + itemSKU + ", itemSkuPrice=" + itemSkuPrice + ", tiChen="
+				+ tiChen + ", itemQb=" + itemQb + ", stockNum=" + stockNum + ", canUse=" + canUse + ", itemValueId="
+				+ itemValueId + ", itemPropertyName=" + itemPropertyName + ", itemPropertyInfo=" + itemPropertyInfo
+				+ ", itemPropertyNameTwo=" + itemPropertyNameTwo + ", itemPropertyTwoValue=" + itemPropertyTwoValue
+				+ ", itemPropertyNameThree=" + itemPropertyNameThree + ", itemPropertyThreeValue="
+				+ itemPropertyThreeValue + ", itemPropertyFourName=" + itemPropertyFourName + ", itemPropertyFourValue="
+				+ itemPropertyFourValue + ", itemPropertyFiveName=" + itemPropertyFiveName + ", itemPropertyFiveValue="
+				+ itemPropertyFiveValue + ", itemPropertySixName=" + itemPropertySixName + ", itemPropertySixValue="
+				+ itemPropertySixValue + ", itemName=" + itemName + ", itemType=" + itemType + ", itemBrandName="
+				+ itemBrandName + ", picPath=" + picPath + "]";
+	}
+    
+}

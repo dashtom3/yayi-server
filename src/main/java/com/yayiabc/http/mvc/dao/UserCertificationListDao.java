5bf711b9597808efa3e631e4cb4719ffd42653d5
zhangpeng <thinking0301@163.com>
2017-09-08 15:08:57 +0800

    15：08提交

--- src/main/java/com/yayiabc/http/mvc/dao/UserCertificationListDao.java
+++ src/main/java/com/yayiabc/http/mvc/dao/UserCertificationListDao.java
@@ -12,10 +12,10 @@ import com.yayiabc.http.mvc.pojo.jpa.User;
 @Repository
 public interface UserCertificationListDao {
 	List<User> list(@Param("phone")String phone,@Param("trueName")String trueName,@Param("companyName")String companyName,@Param("type")Integer type,@Param("state")Integer state,@Param("page")Page page);
-	
-	void verify(@Param("userId")String userId,@Param("state")Integer state,@Param("failReason")String failReason);
-	
+
 	int getCount(@Param("phone")String phone,@Param("trueName")String trueName,@Param("companyName")String companyName,@Param("type")Integer type,@Param("state")Integer state);
+
+	void verify(@Param("userId")String userId,@Param("state")Integer state,@Param("failReason")String failReason);
 
 	void verifyFail(@Param("userId")String userId);	//审核未通过时清空资质信息
 

bc6175d8ddd19a72159e8441bf889e0307d6bac4
liqitian3344 <192253540@qq.com>
2017-10-11 11:50:33 +0800

    1231231231

---
+++ src/main/java/com/yayiabc/http/mvc/pojo/model/SaleWit.java
@@ -0,0 +1,21 @@
+package com.yayiabc.http.mvc.pojo.model;
+
+import java.util.Date;
+
+public class SaleWit {
+	private Date created;
+	private String balance;
+	public Date getCreated() {
+		return created;
+	}
+	public void setCreated(Date created) {
+		this.created = created;
+	}
+	public String getBalance() {
+		return balance;
+	}
+	public void setBalance(String balance) {
+		this.balance = balance;
+	}
+
+}
